932788adea5ec4ad94e48bc377e640668574d50a
WnaesVlamro <valenciawaner@gmail.com>
2020-06-03 04:39:25 -0500

    Updates

---
+++ precache-manifest.4bd77d2bf30bfb8cb821cfca78e3bf81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c0d486ef272f50494cda70aa7ae68ac",
+    "url": "/task-fd/index.html"
+  },
+  {
+    "revision": "5d285e489747b6eeabc2",
+    "url": "/task-fd/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "22f9cc4e1c57ef58e6a5",
+    "url": "/task-fd/static/js/2.f1e78e4e.chunk.js"
+  },
+  {
+    "revision": "1476762afe1aa8b2e1b2ce657d32eb83",
+    "url": "/task-fd/static/js/2.f1e78e4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d285e489747b6eeabc2",
+    "url": "/task-fd/static/js/main.2054ba57.chunk.js"
+  },
+  {
+    "revision": "770e3962bbadee5a9ff1",
+    "url": "/task-fd/static/js/runtime-main.a6c2a09b.js"
+  }
+]);
